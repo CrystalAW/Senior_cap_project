2a39fb5d3aa14abd584bcf0c6d08d583d9111990
CrystalAW <wrightca5@appstate.edu>
2025-03-18 15:36:32 -0400

    pushing angular basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { CalDayComponent } from './cal-day/cal-day.component';
 import { CalWeekComponent } from './cal-week/cal-week.component';
 import { CalendarComponent } from './calendar/calendar.component';
+import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { ScheduleComponent } from './schedule/schedule.component';
 import { TasklistComponent } from './tasklist/tasklist.component';
-import { HeaderComponent } from './header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'monthly', component: CalendarComponent},
   {path: 'weekly', component: CalWeekComponent},
   {path: 'daily', component: CalDayComponent},
+  {path: 'schedule', component: ScheduleComponent},
+  {path: 'task', component: TasklistComponent}
 ];
 
 @NgModule({
@@ -28,7 +31,8 @@ const routes: Routes = [
     TasklistComponent,
     CalWeekComponent,
     CalDayComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
