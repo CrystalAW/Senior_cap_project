038f7330f0d0b19abb9b769db76ed0e2568afc28
CrystalAW <wrightca5@appstate.edu>
2025-04-21 20:57:54 -0400

    integrating task api

--- app_public/DigiP/src/app/google-calendar.service.ts
+++ app_public/DigiP/src/app/google-calendar.service.ts
@@ -6,17 +6,37 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class GoogleCalendarService {
-  private apiUrl = '/api/calendar';
+  private calUrl = '/api/calendar';
 
   constructor(private http: HttpClient) { }
 
   getEvents(): Observable<any> {
     // check in this file if there are recurring events and add a tag to them
-    return this.http.get<any>(this.apiUrl);
+    return this.http.get<any>(this.calUrl);
   }
 
   addEvent(event: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl, event);
+    return this.http.post<any>(this.calUrl, event);
   }
 
+  //taskslists api methods
+  getTaskLists(): Observable<any> {
+    return this.http.get(`${this.calUrl}/tasklists/`);
+  }
+
+  getTaskfromLists(listId:string, task:any): Observable<any> {
+    return this.http.get(`${this.calUrl}/tasklists/${listId}/tasks`, task);
+  }
+
+  createTask(listId:string, task:any): Observable<any> {
+    return this.http.post(`${this.calUrl}/tasklists/${listId}/tasks`, task);
+  }
+
+  completeTask(listId: string, taskId: string): Observable<any> {
+    return this.http.patch(`${this.calUrl}/tasklists/${listId}/tasks/${taskId}/complete}`, {});
+  }
+
+  deleteTask(listId:string, taskId:string): Observable<any> {
+    return this.http.delete(`${this.calUrl}/tasklists/${listId}/tasks/${taskId}`);
+  }
 }
